326decd774c8aa5be3378fc4769b9fe52298bee3
ProCode2 <iamprobablycoding@gmail.com>
2022-04-15 01:00:17 +0530

    added cart page, started login and register page styling

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,28 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.render("home");
-})
-
 // set assets folder
 app.use(express.static("public"))
 // set template engine
 app.use(expressLayouts);
 app.set('views', path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+})
+
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+})
+
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+})
+
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+})
 
 app.listen(PORT, () => {
   console.log(`App running at http://localhost:${PORT}`)
