b562d86df01594515c32c675dee9819b44814f9d
ProCode2 <iamprobablycoding@gmail.com>
2022-04-15 19:43:58 +0530

    added login, logout backend authentication

--- backend/routes/web.js
+++ backend/routes/web.js
@@ -2,14 +2,21 @@ const homeController = require("../app/http/controllers/homeController")
 const authController = require("../app/http/controllers/authController")
 const cartController = require("../app/http/controllers/customers/cartController")
 
+// middlewares
+// stop logged in users from accessing login, register routes
+const guest = require("../app/http/middlewares/guest");
+
 const initRoutes = (app) => {
 
-  app.get("/", homeController().index)
-  app.get("/login", authController().login)
-  app.get("/register", authController().register)
+  app.get("/", homeController().index);
+  app.get("/login", guest, authController().login);
+  app.post("/login", authController().postLogin);
+  app.get("/register", guest, authController().register);
+  app.post("/register", authController().postRegister);
+  app.post("/logout", authController().logout);
 
-  app.get("/cart", cartController().index)
-  app.post("/cart/update", cartController().update)
+  app.get("/cart", cartController().index);
+  app.post("/cart/update", cartController().update);
 }
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,11 @@ const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
 const session = require("express-session");
 const pgSession = require('connect-pg-simple')(session)
-const sessionPool = require('pg').Pool
+const sessionPool = require('pg').Pool;
 const flash = require("express-flash");
 const { initRoutes } = require("./routes/web");
+require('dotenv').config();
+const passport = require("passport");
 
 const app = express();
 
@@ -15,11 +17,11 @@ const PORT = process.env.PORT || 3000;
 app.use(flash())
 // session config
 const sessionDBaccess = new sessionPool({
-  user: "postgres",
-  password: "hhhhhh123",
-  host: "localhost",
-  port: "5432",
-  database: "cemk_meals"
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
 })
 const sessionConfig = {
   store: new pgSession({
@@ -27,24 +29,30 @@ const sessionConfig = {
     tableName: 'session'
   }),
   name: 'SID',
-  secret: "thisisverysecret",
+  secret: process.env.COOKIE_SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
-    maxAge: 1000 * 15,
+    maxAge: 1000 * 60 * 60 * 24,
     aameSite: true,
     secure: false // ENABLE ONLY ON HTTPS
   }
 }
 app.use(session(sessionConfig))
+const passportInit = require("./app/config/passport");
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Global middleware
 app.use((req, res, next) => {
   res.locals.session = req.session;
+  res.locals.user = req.user;
   next();
 })
 // set assets folder
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 // set template engine
 app.use(expressLayouts);
